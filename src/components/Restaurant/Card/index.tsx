d2fd4f09fa66a9edf79cb21a5703aa97b9484398
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-10-31 21:25:22 -0500

    updated home styles

--- src/components/Restaurant/Card/index.tsx
+++ src/components/Restaurant/Card/index.tsx
@@ -7,6 +7,16 @@ import { Icon } from "@iconify/react";
 export const RestaurantCard = () => {
     return (
         <div className="w-full flex flex-col items-center text-white max-w-[1000px] ">
+            <div className="w-full">
+                <h4 className="text-2xl font-bold mb-2"> Restaurant Name </h4>
+                <p className="text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, doloremque ...
+                </p>
+                <div className="flex items-end gap-2 my-5">
+                    <Icon icon="carbon:location-filled" fontSize={30} />
+                    <p className="font-bold">Av. de los paltas</p>
+                </div>
+            </div>
             <div className="h-64 w-full flex  gap-4 overflow-x-scroll overscroll-x-auto py-2">
                 <div className="border-4 min-w-[300px] w-64 md:w-1/3 rounded-lg overflow-hidden">
                     <img
@@ -31,8 +41,6 @@ export const RestaurantCard = () => {
                 </div>
             </div>
             <div className="w-full mt-4">
-                <h4 className="text-2xl font-bold"> Restaurant Name </h4>
-                <p>Lorem ipsum dolor sit amet...</p>
                 <div className="flex gap-4 mt-4">
                     <div className="">
                         <Icon icon="ri:whatsapp-fill" fontSize={40} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,27 @@ import { RestaurantCard } from "../components/Restaurant/Card";
 const Home = () => {
     return (
         <>
+            <section className="h-screen bg-rose-500">
+                <div className="container flex flex-col items-center justify-center h-full">
+                    <RestaurantCard />
+                </div>
+            </section>
+            <section className="h-screen bg-zinc-800">
+                <div className="container flex flex-col items-center justify-center h-full">
+                    <RestaurantCard />
+                </div>
+            </section>
             <section className="h-screen bg-lime-600">
                 <div className="container flex flex-col items-center justify-center h-full">
                     <RestaurantCard />
                 </div>
             </section>
             <section className="h-screen bg-zinc-800">
+                <div className="container flex flex-col items-center justify-center h-full">
+                    <RestaurantCard />
+                </div>
+            </section>
+            <section className="h-screen bg-orange-600">
                 <div className="container flex flex-col items-center justify-center h-full">
                     <RestaurantCard />
                 </div>
