4484ca8bfc0bf34fa3a3148ea20c6535919b6798
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-10-31 22:43:16 -0500

    updated styles

--- src/components/Restaurant/Card/index.tsx
+++ src/components/Restaurant/Card/index.tsx
@@ -11,30 +11,20 @@ export const RestaurantCard = ({ restaurant }: RestaurantCardProps) => {
             <div className="w-full">
                 <h4 className="text-2xl font-bold mb-2"> {restaurant?.name} </h4>
 
-                <div className="flex items-center gap-2 my-5 hover:scale-105 duration-200 cursor-pointer">
-                    <Icon icon="carbon:location-filled" fontSize={20} />
+                <a
+                    href={restaurant?.location_link}
+                    target="_blank"
+                    rel="noopener"
+                    className="flex items-center gap-2 my-5 hover:scale-105 duration-200 cursor-pointer"
+                >
+                    <Icon icon="carbon:location-filled" fontSize={20} className="animate-bounce" />
                     <p className="font-bold capitalize">{restaurant?.full_address}</p>
-                    <Icon icon="mingcute:arrow-right-fill" className="animate-bounce" />
-                </div>
+                </a>
             </div>
-            <div className="h-64 w-full flex  gap-4 overflow-x-scroll overscroll-x-auto py-2">
-                <div className="border-4 min-w-[300px] w-64 md:w-1/3 rounded-lg overflow-hidden">
+            <div className="h-64 md:h-80 w-full flex  gap-4 overflow-x-scroll overscroll-x-auto py-2">
+                <div className="border-4 min-w-[300px] w-full md:w-96 rounded-lg overflow-hidden">
                     <img
                         src={restaurant?.photo}
-                        alt="image"
-                        className="w-full h-full object-cover hover:scale-105 hover:rotate-3 duration-200"
-                    />
-                </div>
-                <div className="border-4 min-w-[300px] w-64 md:w-1/3 rounded-lg overflow-hidden">
-                    <img
-                        src={restaurant?.street_view}
-                        alt="image"
-                        className="w-full h-full object-cover hover:scale-105 hover:rotate-3 duration-200"
-                    />
-                </div>
-                <div className="border-4 min-w-[300px] w-64 md:w-1/3 rounded-lg overflow-hidden">
-                    <img
-                        src="https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?auto=format&fit=crop&q=80&w=2487&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                         alt="image"
                         className="w-full h-full object-cover hover:scale-105 hover:rotate-3 duration-200"
                     />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Icon } from "@iconify/react";
 import data from "../data/data.json";
 
 const Home = () => {
+    const colors = ["bg-rose-600", "bg-zinc-800", "bg-lime-600", "bg-zinc-800", "bg-orange-600"];
     return (
         <>
             <div className="fixed right-4 bottom-12">
@@ -10,13 +11,18 @@ const Home = () => {
                     <Icon icon="ep:arrow-up-bold" />
                 </div>
             </div>
-            {data?.map((restaurant) => (
-                <section className="h-screen bg-zinc-800">
-                    <div className="container flex flex-col items-center justify-center h-full">
-                        <RestaurantCard restaurant={restaurant} />
-                    </div>
-                </section>
-            ))}
+            {data?.map((restaurant, index: number) => {
+                const i = index;
+                const n = colors.length;
+                const color = colors[((i % n) + n) % n];
+                return (
+                    <section className={`h-screen ${color}`} key={`r-${index}`}>
+                        <div className="container flex flex-col items-center justify-center h-full">
+                            <RestaurantCard restaurant={restaurant} />
+                        </div>
+                    </section>
+                );
+            })}
         </>
     );
 };
